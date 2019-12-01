dbfa11620dd9d02c09b5133f0f88c34b6940a31c
flpsantos3 <56131158+flpsantos3@users.noreply.github.com>
2019-12-01 22:51:13 +0000

    Corrected file name
    Corrected created file name to correctly reflect the scope of the original file

--- writeFiles.py
+++ writeFiles.py
@@ -6,7 +6,7 @@
 
 def writeHeader(fileName):
     """Uses the header info from fileName to write a new file
-    Requires: fileName is str, the name of a .txt file with time, day, company info
+    Requires: fileName is str, the name of a .txt file with time, day, company name and scope of the file
     Returns: a .txt file with the updated info
     """
 
@@ -28,16 +28,29 @@ def writeHeader(fileName):
                                         #day, month, year
     day = ddmmyy[0]
     month = ddmmyy[1] 
-    year = ddmmyy[2] 
+    year = ddmmyy[2]
 
-    new_name = "drones" + time_next + "_" + year + "y" + month + "m" + day + ".txt"
+    scope = info[3].lower().replace(":","") #removes ":" from scope
+
+    new_name = scope + time_next + "_" + year + "y" + month + "m" + day + ".txt"
 
     outputfile = open(new_name, 'w')
 
     company = info[2]
 
-    header = "Time:\n" + time_next + "\n" + "Day:\n" + date_next + "\n" + "Company:\n" + company + "\n"
+    header = "Time:\n" + time_next + "\n" + "Day:\n" + date_next + "\n" + "Company:\n" + company + "\n" + info[3] + "\n"
 
     outputfile.write(header)
 
     outputfile.close()
+
+def writeTimeline(pairings):
+    """Creates a new .txt with the paired drones and parcels
+    Requires: pairings is a list where each element is a drone and parcel pairing
+    Ensures: a .txt file with the paired drones and parcels, where each line corresponds
+    to a pairing
+    """
+
+
+
+    
