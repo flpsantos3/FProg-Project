ebbd99d304417b482a8259c15973e3d0c2003331
flpsantos3 <56131158+flpsantos3@users.noreply.github.com>
2019-12-01 22:50:02 +0000

    Update readFiles.py
    Updated readDronesFile to return a list with header and lists for each of the drones

--- readFiles.py
+++ readFiles.py
@@ -11,15 +11,32 @@ def readDronesFile(fileName):
     
     Requires: fileName is str, the name of a .txt file listing drones,
     following the format specified in the project sheet.
-    Ensures: list whose first element is ... <to complete> (the 8th line of the drone file, followed by the characteristics of the remaining available drones)??
+    Ensures: list whose first element is the header of fileName, followed by
+    lists corresponding to the drones and their characteristics
     """
     outputList = []
+
+    header = readHeader(fileName)
     
-    outputList.append(readHeader(fileName))
+    outputList.append(header)
     
     fileIn = open(fileName, 'r')
 
-    # ... <to complete>
+    drones = list(fileIn)[8:]
+
+    i = 0
+    for i in range(len(drones)):
+        drones[i] = drones[i][:-1]
+        drones[i] = drones[i].split(", ")
+        i = i + 1
+
+    #each drone is represented by a list with the format [name, operating zone,
+    #max weight, max range from base (m), total distance (km), autonomy (km),
+    #date of availability yyyy-mm-dd, time of availability (hh:mm)]
+
+    outputList.extend(drones)
+
+    fileIn.close()
 
     return outputList
 
@@ -27,7 +44,7 @@ def readDronesFile(fileName):
 
 def readHeader(fileName):
     """
-    Reads a txt file with the drones list and returns the day, time, company and scope of the file
+    Reads a .txt file and returns the day, time, company and scope of the file
     Requires: fileName is str, the name of a .txt file listing drones,
     following the format specified on the project
     Ensures: returns a tuple with the day, time, company and scope of fileName
@@ -41,13 +58,10 @@ def readHeader(fileName):
     day = fileIn.readline().strip().replace("\n", "")
     fileIn.readline()
     company = fileIn.readline().strip().replace("\n", "")
-    fileIn.readline()
-    scope = fileIn.readlines()
+    #fileIn.readline()
+    scope = fileIn.readline().strip().replace("\n", "")
 
-    i = 0
-    for i in range(len(scope)):
-        scope[i] = scope[i][:-1]
-        i = i + 1
+    fileIn.close()
     
-    return time, day, company, scope
+    return (time, day, company, scope)
     
