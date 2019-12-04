7d0db43772b35925fc4d72e7cf72d2f405792369
lflnunes <49460696+lflnunes@users.noreply.github.com>
2019-12-04 18:10:51 +0000

    constante coluna test

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+
+
 # 2019-2020 Fundamentos de Programação
 # Grupo 20
 # 55142 Filipe Santos
@@ -6,3 +8,16 @@
 time = ["8h00","8h30","9h00","9h30","10h00","10h30","11h00","11h30","12h00",
 "12h30","13h00","13h30","14h00","14h30","15h00","15h30","16h00","16h30",
 "17h00","17h30","18h00","18h30","19h00","19h30","20h00"]
+
+parcels=[['Daenerys Targaryen', 'lumiar', '2019-11-05', '11:00', '900', '15', '20'],['John Snow', 'lumiar', '2019-11-05', '11:05', '1200', '10', '30']]
+drones=[['Jupiter', 'alvalade', '20', '2000', '500', '20', '2019-11-05', '10:15'],['Terra', 'ameixoeira', '15', '1500', '400', '20', '2019-11-05', '10:20']]
+
+#droneDistanciaMax=drones[][3]
+parcelDistanciaBase=parcels[0][3]
+
+
+def droneDistanciaMax (linha):
+    return drones[linha][3]
+    
+print(droneDistanciaMax(0))
+
