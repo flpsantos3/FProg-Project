b3d09d36eb19b5c2c38d74e91520b3f274b0d4a6
flpsantos3 <56131158+flpsantos3@users.noreply.github.com>
2019-12-13 20:15:09 +0000

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -1,12 +1,15 @@
 import readFiles
 import writeFiles
 import organize
-
 from operator import itemgetter
+import constants
+import dronyD
+
 
 #print(readFiles.readHeader("drones11h00_2019y11m5.txt"))
 
 #drones = readFiles.readDronesFile("drones11h00_2019y11m5.txt")
+#print(drones)
 
 #drones.pop(0)
 
@@ -16,15 +19,21 @@ from operator import itemgetter
 
 #writeFiles.writeHeader("drones11h00_2019y11m5.txt")
 
-a = "Jupiter, alvalade, 20, 2000, 500.0, 20.0, 2019-11-05, 10:15"
+#a = "Jupiter, alvalade, 20, 2000, 500.0, 20.0, 2019-11-05, 10:15"
 
-drone = a.split(", ")
+#drone = a.split(", ")
 
-b = "Daenerys Targaryen, lumiar, 2019-11-05, 11:00, 900, 15, 20"
+#b = "Daenerys Targaryen, lumiar, 2019-11-05, 11:00, 900, 15, 20"
 
-parcel = b.split(", ")
+#parcel = b.split(", ")
 
 #print(organize.updateDrone(parcel,drone))
 
-print(organize.pairD(parcel,drone))
+#print(organize.pairD(parcel,drone))
 
+#writeFiles.writeHeader("drones11h00_2019y11m5.txt")
+
+#unit tests:    
+#print(constants.dMaxDistM(0), constants.pWeightKg(1))
+
+dronyD.allocate("drones11h00_2019y11m5.txt","parcels11h00_2019y11m5.txt")
